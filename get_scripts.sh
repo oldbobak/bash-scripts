199a03d2ec31a292144ad55d37ca98da5f64f705
oldbobak <59226185+oldbobak@users.noreply.github.com>
2020-11-23 00:51:21 +0300

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-#vs@Duke 20201111
-#load files from github repository and add to home directory
+#  vs@Duke 20201123
+#  load files from github repository and add to home directory
 #  wget https://raw.githubusercontent.com/oldbobak/bash-scripts/master/
-# run it using
+#  run it using
 #     wget https://raw.githubusercontent.com/oldbobak/bash-scripts/master/get_scripts.sh -O - | bash
-
 
 # check if wget exist in system
 command -v wget >/dev/null 2>&1 || { echo >&2 "$0 require wget but it's not installed.  Aborting."; exit 1; }
 
-
 wget https://raw.githubusercontent.com/oldbobak/bash-scripts/master/screenrc -O ~/.screenrc
 wget https://raw.githubusercontent.com/oldbobak/bash-scripts/master/bash_aliases -O ~/.bash_aliases
+wget https://raw.githubusercontent.com/oldbobak/bash-scripts/master/vimrc -O ~/.vimrc
 
+#check if we've appended to .bashrc
 #if [ -f ~/.bashrc ] && [ grep Duke ~/.bashrc ]
 grep Duke ~/.bashrc  > /dev/null
-
 if [ $? -eq 0 ]
 then
     echo "File .bashrc was processd, skiping.."
